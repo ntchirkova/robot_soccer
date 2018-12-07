dafbc2257fc80c8e7933ecc5a7f4551c304255a4
abuchele <anna.buchele@students.olin.edu>
2018-12-07 14:10:03 -0500

    finding round and green things, instead of only round or only green

--- src/find_ball.py
+++ src/find_ball.py
@@ -1,105 +1,5 @@
 #!/usr/bin/env python
 
-import os, sys
-from os import path
-import csv
-import cv2 as cv
-import numpy as np
-
-resize = (160, 120)
-boxlist = []
-
-def rad2box(x, y, rad):
-    """
-    Returns the coordinates of the upper left and
-    bottom right coordinates of the bounding box
-    for the ball coordinates based on
-    the radius of the ball and center coordinates
-    """
-    offset = float(rad)
-    box = [x - offset, y + offset, x + offset, y - offset]
-    return box
-
-def getAngleDist(x,radius):
-    """
-    Returns the angle and distance to the ball from
-    the neato's current position
-    """
-    BALL_DI = 7.5 #ball is 7.5 inches in diameter.
-    FOV = 60. #Field of view in degrees.
-    FOCAL = 150.*12./BALL_DI #The first number is the measured width in pixels of a picture taken at the second number's distance (inches).
-    center = resize[0]/2
-    difference = int(x) - center
-    distance = BALL_DI * FOCAL / float(2.*radius)
-    #Because the camera isn't a 1:1 camera, it has a 60 degree FoV, which makes angle calculations easier because angle
-    #is directly proportional to distance from center.
-    angle = float(difference)/160. * (FOV/2.) #scale to half of FoV
-
-    return angle, distance
-
-for filename in os.listdir("data"):
-    if filename.endswith(".png") or filename.endswith(".JPG") or filename.endswith(".jpg"):
-        print(str(filename))
-        filepath = os.path.join("data", filename)
-        base = cv.imread(filepath, 1)
-        base = cv.resize(base, resize, interpolation = cv.INTER_AREA)
-        img = cv.medianBlur(base.copy(),5)
-        img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-
-        lower = np.array([40, 10, 10])
-        upper = np.array([110, 255, 255])
-
-        img = cv.inRange(img, lower, upper)
-        img = cv.erode(img, None, iterations=3)
-        img = cv.dilate(img, None, iterations=2)
-
-        contours = cv.findContours(img.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[-2]
-        center = None
-
-        # Draw bounding circle around largest contour
-        if len(contours) > 0:
-            largest_contour = max(contours, key=cv.contourArea)
-            ((x, y), radius) = cv.minEnclosingCircle(largest_contour)
-
-            # Draw circles on image to represent the ball
-            if radius > 10:
-                # cv.circle(base, (int(x), int(y)), int(radius),
-                #     (0, 255, 255), 2)
-                # cv.circle(base, (int(x), int(y)), 5,
-                #     (0, 0, 255), -1)
-                print("coord:" + str(x) + "," + str(y) + " radius:" + str(radius))
-                angle, dist = getAngleDist(float(x), float(radius))
-                print("angle:" + str(angle) + " distance:" + str(dist))
-
-                box = rad2box(float(x), float(y), float(radius))
-                box.append(radius)
-                box.append(float(angle))
-                box.append(dist)
-                cv.rectangle(base, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255,0,0), 2)
-                boxlist.append(box)
-
-
-                cv.line(base, (int(x), int(y)), (160, 240), (255,0,0), 1, 8, 0)
-
-            else:
-                print("Radius too small!")
-        continue
-
-    else:
-        continue
-
-
-
-with open('ball_info.csv', mode='w') as ball_info:
-    ball_writer = csv.writer(ball_info, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for i in boxlist:
-        ball_writer.writerow([i[0]] + [i[1]] + [i[2]] + [i[3]] + [i[4]] + [i[5]] + [i[6]])
-
-
-
-cv.imshow('detected circles', base)
-cv.waitKey(25)
-cv.destroyAllWindows()
 import cv2
 import numpy as np
 
@@ -182,56 +82,59 @@ def find_ball(base):
 		outimg = cv2.erode(outimg, None, iterations=3)
 		outimg = cv2.dilate(outimg, None, iterations=2)
 
-		if method == 0:
+		contours = cv2.findContours(outimg.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+		center = None
 
-			contours = cv2.findContours(outimg.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-			center = None
+		# Draw bounding circle around largest contour
+		if len(contours) > 0:
+			largest_contour = max(contours, key=cv2.contourArea)
+			((contour_x, contour_y), contour_r) = cv2.minEnclosingCircle(largest_contour)
 
-			# Draw bounding circle around largest contour
-			if len(contours) > 0:
-				largest_contour = max(contours, key=cv2.contourArea)
-				((x, y), radius) = cv2.minEnclosingCircle(largest_contour)
+			# Draw circles on image to represent the ball
+			if contour_r > 10:
+				#print "coord:" + str(x) + "," + str(y) + " radius:" + str(radius)
+				angle, dist = getAngleDist(float(contour_x), float(contour_r))
+				#print "angle:" + str(angle) + " distance:" + str(dist)
 
-				# Draw circles on image to represent the ball
-				if radius > 10:
-					#print "coord:" + str(x) + "," + str(y) + " radius:" + str(radius)
-					angle, dist = getAngleDist(float(x), float(radius))
-					#print "angle:" + str(angle) + " distance:" + str(dist)
+				box = rad2box(float(contour_x), float(contour_y), float(contour_r))
+				box.append(contour_r)
+				box.append(float(angle))
+				box.append(dist)
+				cv2.rectangle(rectimg, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255,0,0), 2)
+				boxlist.append(box)
 
-					box = rad2box(float(x), float(y), float(radius))
-					box.append(radius)
-					box.append(float(angle))
-					box.append(dist)
-					cv2.rectangle(rectimg, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255,0,0), 2)
-					boxlist.append(box)
+				#cv2.line(base, (int(x), int(y)), (160, 240), (255, 0, 0), 1, 8, 0)
+				visimg = cv2.cvtColor(outimg,cv2.COLOR_GRAY2RGB)
+				vis = np.concatenate((visimg, rectimg), axis=1)
 
-					#cv2.line(base, (int(x), int(y)), (160, 240), (255, 0, 0), 1, 8, 0)
-					visimg = cv2.cvtColor(outimg,cv2.COLOR_GRAY2RGB)
-					vis = np.concatenate((visimg, rectimg), axis=1)
-		else:
-			gray = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
-			# gray = cv2.bitwise_and(gray, gray, mask= outimg)
-			# detect circles in the image
-			circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1.2, 100)
-			 
-			# ensure at least some circles were found
-			if circles is not None:
-				# convert the (x, y) coordinates and radius of the circles to integers
-				circles = np.round(circles[0, :]).astype("int")
-			 
-				# loop over the (x, y) coordinates and radius of the circles
-				for (x, y, r) in circles:
+		gray = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
+		# gray = cv2.bitwise_and(gray, gray, mask= outimg)
+		# detect circles in the image
+		circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+		 
+		# ensure at least some circles were found
+		if circles is not None:
+			# convert the (x, y) coordinates and radius of the circles to integers
+			circles = np.round(circles[0, :]).astype("int")
+		 
+			# loop over the (x, y) coordinates and radius of the circles
+			for (x, y, r) in circles:
+				min_x = x - r
+				max_x = x + r
+
+				if (max_x > contour_x > min_x):
 					# draw the circle in the output image, then draw a rectangle
 					# corresponding to the center of the circle
 					cv2.circle(rectimg, (x, y), r, (0, 255, 0), 4)
 					cv2.rectangle(rectimg, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-			 
-				# show the output image
-				visimg = cv2.cvtColor(outimg, cv2.COLOR_GRAY2RGB)
-				vis = np.concatenate((visimg, rectimg), axis=1)
-			else:
-				visimg = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
-				vis = np.concatenate((visimg,rectimg), axis=1)
+		 
+			# show the output image
+			visimg = cv2.cvtColor(outimg, cv2.COLOR_GRAY2RGB)
+			vis = np.concatenate((visimg, rectimg), axis=1)
+		else:
+			visimg = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+			vis = np.concatenate((visimg,rectimg), axis=1)
+
 
 		cv2.imshow('image', vis)
 		k = cv2.waitKey(1) & 0xFF
@@ -277,6 +180,6 @@ def check_channels(img):
 
 
 if __name__ == '__main__':
-	img = cv2.imread('ball.png')
-	#find_ball(img)
-	check_channels(img)
+	img = cv2.imread('ball_distraction.png')
+	find_ball(img)
+	#check_channels(img)
